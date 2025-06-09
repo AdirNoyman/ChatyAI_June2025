b88230994c82013ccd2e7b06b3663bfe1233e1a0
Adir Noyman <ofahol2007@hotmail.com>
2025-06-09 20:32:44 +0300

    first commit

---
+++ chaty/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="">
+      <h1 className='text-4xl font-bold text-purple-500'>Hello Adiros 🙋‍♂️🤓</h1>
+      <Button variant="default" size="lg" className="mt-4">
+        Click Me!
+      </Button>      
+    </div>
+  )
+}
+
+export default Home
